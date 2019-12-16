5adb49deb1d3959c6e7d0088e6345c07d0924148
zijian <zijiankao@gmail.com>
2019-12-16 16:28:06 +0800

    asd

--- flask_test.py
+++ flask_test.py
@@ -2,13 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
-
 def hello_world():
 	return "hello world"
+
+@app.route("/test/<param>")
+def search(param):
+	return param
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower == "mike":
+		return "hello,{}".format(name)
+	else:
+		return "not found", 404
 
 if __name__ == "__main__":
 	app.run()
 
-	
